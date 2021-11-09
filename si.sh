82ca90fabd92e813a83d5429d1792ecaf5bd0d28
scottgai <scottgai@gmail.com>
2021-11-09 10:12:01 +0800

    add some checks

--- si.sh
+++ si.sh
@@ -12,6 +12,25 @@ printpage()
 }
 
 ## main 
+# check if cf/jq exist
+which cf > /dev/null 2>&1 ; existing=$?;
+if [ $existing -ne 0 ]; then
+    echo "cf CLI not available. Please install it or set PATH env variable properly!"
+    exit 1
+fi
+
+which jq > /dev/null 2>&1 ; existing=$?;
+if [ $existing -ne 0 ]; then
+    echo "jq utility not available. Please install it or set PATH env variable properly!"
+    exit 1
+fi
+
+# check if cf logged in
+cf orgs > /dev/null 2>&1 ; login_status=$?;
+if [ $login_status -ne 0 ]; then
+    echo "Not logged in yet. Please do 'cf login' first!"
+    exit 1
+fi
 ## get first page
 onepage=$(cf curl /v2/service_instances)
 next_url=$(echo $onepage | jq -r '.next_url')
